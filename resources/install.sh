133a669916484899a7d3632aef075537ac0cf134
Alexandre JANNOTTA <wlmitch@gmail.com>
2020-04-13 16:20:13 +0200

    change quotes

--- resources/install.sh
+++ resources/install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 touch /tmp/dependency_panasonicVIERA_in_progress
-echo "Launch install of PanasonicViera dependency"
+echo 'Launch install of PanasonicViera dependency'
 
 echo 50 > /tmp/dependency_panasonicVIERA_in_progress
 echo 'Install panasonic-viera library'
 pip3 install panasonic-viera
 
 echo 100 > /tmp/dependency_panasonicVIERA_in_progress
-echo "Everything is successfully installed!"
+echo 'Everything is successfully installed!'
 rm /tmp/dependency_panasonicVIERA_in_progress
