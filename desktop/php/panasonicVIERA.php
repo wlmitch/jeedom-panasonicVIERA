eb71ff4cb595a8eb2807ba5bdb926c984f764c7e
Alexandre JANNOTTA <wlmitch@gmail.com>
2020-04-14 14:59:24 +0200

    Remove Tab

--- desktop/php/panasonicVIERA.php
+++ desktop/php/panasonicVIERA.php
@@ -57,7 +57,6 @@ $eqLogics = eqLogic::byType('panasonicVIERA');
         <ul class="nav nav-tabs" role="tablist">
             <li role="presentation"><a href="#" class="eqLogicAction" aria-controls="home" role="tab" data-toggle="tab" data-action="returnToThumbnailDisplay"><i class="fa fa-arrow-circle-left"></i></a></li>
             <li role="presentation" class="active"><a href="#eqlogictab" aria-controls="home" role="tab" data-toggle="tab"><i class="fa fa-tachometer"></i> {{Equipement}}</a></li>
-            <li role="presentation" class="expertModeVisible"><a href="#featuretab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-id-card-o"></i> {{Caractéristiques}}</a></li>
             <li role="presentation"><a href="#commandtab" aria-controls="profile" role="tab" data-toggle="tab"><i class="fa fa-list-alt"></i> {{Commandes}}</a></li>
         </ul>
         <div class="tab-content" style="height:calc(100% - 50px);overflow:auto;overflow-x: hidden;">
