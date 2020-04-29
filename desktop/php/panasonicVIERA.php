1b9fd277c90d29bbee126782a5307fea85b648c0
Alexandre JANNOTTA <wlmitch@gmail.com>
2020-04-29 21:03:02 +0200

    jeedom v4

--- desktop/php/panasonicVIERA.php
+++ desktop/php/panasonicVIERA.php
@@ -79,7 +79,7 @@ $eqLogics = eqLogic::byType('panasonicVIERA');
                             <div class="col-sm-3">
                             	<select id="sel_object" class="eqLogicAttr form-control" data-l1key="object_id">
                                 	<option value="">{{Aucun}}</option>
-    <?php foreach (object::all() as $object) : ?>
+    <?php foreach (jeeObject::all() as $object) : ?>
                         		<option value="<?= $object->getId() ?>"><?= $object->getName() ?></option>
     <?php endforeach; ?>
                             	</select>
